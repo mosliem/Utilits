d91a751204ebd234738cf04cfd09abf3966a3854
mohamed sliem <mohamedsliem@Mohameds-MacBook-Air.local>
2024-03-12 17:20:01 +0200

    Added TableView extension

---
+++ Sources/Extension/UITableView/UITableView+Dequeue.swift
@@ -0,0 +1,46 @@
+//
+//  UITableView+Dequeue.swift
+//
+//
+//  Created by mohamed sliem on 12/03/2024.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func dequeueReusableCell<T: UITableViewCell>(withClass name: T.Type) -> T {
+        
+        guard let cell = dequeueReusableCell(withIdentifier: String(describing: name)) as? T else {
+            fatalError(
+                "Couldn't find UITableViewCell for \(String(describing: name))"
+            )
+        }
+        return cell
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell> (
+        withClass name: T.Type,
+        for indexPath: IndexPath
+    ) -> T {
+        
+        guard let cell = dequeueReusableCell(withIdentifier: String(describing: name), for: indexPath) as? T else {
+            fatalError(
+                "Couldn't find UITableViewCell for \(String(describing: name))"
+            )
+        }
+        return cell
+    }
+    
+    func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(withClass name: T.Type) -> T {
+        
+        guard let headerFooterView = dequeueReusableHeaderFooterView(
+            withIdentifier: String(describing: name)
+        ) as? T else {
+            fatalError("Couldn't find UITableViewHeaderFooterView for \(String(describing: name))")
+        }
+        return headerFooterView
+    }
+    
+    
+}
---
+++ Sources/Extension/UITableView/UITableView+Register.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by mohamed sliem on 12/03/2024.
+//
+
+import UIKit
+
+extension UITableView {
+    
+     func register<T: UITableViewHeaderFooterView>(nib: UINib?, withHeaderFooterViewClass name: T.Type) {
+          register(nib, forHeaderFooterViewReuseIdentifier: String(describing: name))
+      }
+
+      func register<T: UITableViewHeaderFooterView>(headerFooterViewClassWith name: T.Type) {
+          register(T.self, forHeaderFooterViewReuseIdentifier: String(describing: name))
+      }
+
+      func register<T: UITableViewCell>(cellWithClass name: T.Type) {
+          register(T.self, forCellReuseIdentifier: String(describing: name))
+      }
+
+      func register<T: UITableViewCell>(nib: UINib?, withCellClass name: T.Type) {
+          register(nib, forCellReuseIdentifier: String(describing: name))
+      }
+
+      func register<T: UITableViewCell>(nibWithCellClass name: T.Type, at bundleClass: AnyClass? = nil) {
+          let identifier = String(describing: name)
+          var bundle: Bundle?
+
+          if let bundleName = bundleClass {
+              bundle = Bundle(for: bundleName)
+          }
+
+          register(UINib(nibName: identifier, bundle: bundle), forCellReuseIdentifier: identifier)
+      }
+}
