3f000465e2bbd4906e7feb3e54460db5389e3adf
mohamed sliem <mohamedsliem@Mohameds-MacBook-Air.local>
2024-03-18 00:01:38 +0200

    Added http methods

---
+++ Sources/NetworkLayer/APIClient/HTTPMethods.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethods.swift
+//
+//
+//  Created by mohamed sliem on 17/03/2024.
+//
+
+import Foundation
+
+public enum HTTPMethods: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
