29769f1c42c4e4f9c28f334593448a28db015293
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2024-05-18 21:40:26 +0300

    Added other swift lang versions

--- Package.swift
+++ Package.swift
@@ -6,28 +6,41 @@ import PackageDescription
 let package = Package(
     name: "Utilits",
     platforms: [
-        .iOS(.v14)
+        .iOS(
+            .v14
+        )
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Utilits",
-            targets: ["Utilits"]),
+            targets: ["Utilits"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0"))
+        .package(
+            url: "https://github.com/onevcat/Kingfisher.git",
+            .upToNextMajor(
+                from: "7.0.0"
+            )
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Utilits",
+            name: "Utilits"
+            ,
             dependencies: [
                 "Kingfisher"
             ]
         ),
         .testTarget(
             name: "UtilitsTests",
-            dependencies: ["Utilits"]),
-    ]
+            dependencies: ["Utilits"]
+        ),
+    ],
+    swiftLanguageVersions: [.version(
+        "5.7"
+    )]
 )
