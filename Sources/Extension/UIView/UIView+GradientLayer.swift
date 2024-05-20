89b6ca28ff7f55b6d9ccfce009e9a75f9b6b8f41
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2024-05-20 14:16:25 +0300

    Added Gradient Layer

---
+++ Sources/Extension/UIView/UIView+GradientLayer.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//  
+//
+//  Created by mohamed sliem on 20/05/2024.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func addHorizonalGradientLayer(
+        with colors: [UIColor],
+        locations: [NSNumber]
+    ) {
+        let gradient = CAGradientLayer()
+        gradient.type = .axial
+        
+        gradient.colors = colors
+        gradient.locations = locations
+        
+        gradient.startPoint = CGPoint(x: 0, y: 1)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        
+        gradient.frame = self.frame
+        self.layer.addSublayer(gradient)
+    }
+    
+    func addVerticalGradientLayer(
+        with colors: [UIColor],
+        locations: [NSNumber]
+    ) {
+        let gradient = CAGradientLayer()
+        gradient.type = .axial
+        
+        gradient.colors = colors
+        gradient.locations = locations
+        
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        
+        gradient.frame = self.frame
+        self.layer.addSublayer(gradient)
+    }
+}
