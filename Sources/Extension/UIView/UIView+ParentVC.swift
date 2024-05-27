0fed1c7fa3d2c36a4ade4dd9a05529b970080545
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2024-05-27 14:13:14 +0300

    Added Parent vc extension to UIView

---
+++ Sources/Extension/UIView/UIView+ParentVC.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by mohamed sliem on 27/05/2024.
+//
+
+import UIKit
+
+extension UIView {
+    var parentViewController: UIViewController? {
+        var parentResponder: UIResponder? = self
+        while parentResponder != nil {
+            parentResponder = parentResponder!.next
+            if let viewController = parentResponder as? UIViewController {
+                return viewController
+            }
+        }
+        return nil
+    }
+}
